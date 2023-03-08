580cf8e5b0e012a851b6ae1173e0c2e37803a7fc
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-08 18:20:58 -0500

    Creando vistas[task_detail, task_completed, task_pending]

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -22,7 +22,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('tasks/', views.tasks, name='tasks'),
-    path('tasks/create/', views.task_create, name='task_create'),
+    path('tasks_completed/', views.tasks_completed, name='tasks_completed'),
+    path('tasks_create/', views.task_create, name='task_create'),
+    path('tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('tasks/<int:task_id>/complete', views.complete_task, name='complete_task'),
+    path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
     path('logout/', views.cerrarSesion, name='logout'),
     path('signin/', views.iniciarSesion, name='signin'),
 ]
