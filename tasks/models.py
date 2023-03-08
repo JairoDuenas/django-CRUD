7d988fffde9ba980da1f1b251484037c79759fe1
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-08 09:14:04 -0500

    Creando modelo Task, se registra al panel de admin

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.TextField(blank=True)
+  created = models.DateTimeField(auto_now_add=True)
+  datecomplated = models.DateTimeField(null=True)
+  important = models.BooleanField(default=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  class Meta:
+    verbose_name='task'
+    verbose_name_plural='tasks'
+
+  def __str__(self):
+    return f"{self.title} - by {self.user.username}"
