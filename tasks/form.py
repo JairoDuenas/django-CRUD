23466248e052f8ced1b20a947d2ad8e0248ed251
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-09 14:00:40 -0500

    Dando estilos [task_create, task_detail]

--- tasks/form.py
+++ tasks/form.py
@@ -5,3 +5,8 @@ class TaskForm(forms.ModelForm):
   class Meta:
     model = Task
     fields = ['title', 'description', 'important']
+    widgets = {
+      'title':forms.TextInput(attrs={'class':'form-control my-2', 'placeholder':'Write a title '}),
+      'description':forms.Textarea(attrs={'class':'form-control my-2, ', 'placeholder':'Write a description'}),
+      'important':forms.CheckboxInput(attrs={'class':'form-check-input my-2'})
+    }
